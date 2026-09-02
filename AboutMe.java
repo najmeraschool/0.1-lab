5feb285b432bf1263fedb8806091002c5f2f2aac
nateajmera <nateajmera@gmail.com>
2026-09-02 13:35:35 -0700

    Finish

--- AboutMe.java
+++ AboutMe.java
@@ -1,7 +1,8 @@
 public class AboutMe {
     public static void main(String[] args) {
-        System.out.println("Name: Nathang");
+        System.out.println("Name: Nathan");
         System.out.println("Favorite Book: Wings of Fire");
+        System.out.println("Hobbies: Rowing, piano, and vibecoding");
         System.out.println("Birthday: April 2, 2011");
         System.out.println("Favorite movie: Spider-Man: Brand New Day");
     }
