baded6fa671cf082c617d37db186df007c3b7081
nateajmera <nateajmera@gmail.com>
2026-09-02 13:19:02 -0700

    Work

---
+++ AboutMe.java
@@ -0,0 +1,6 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println(
+                "Name: Nathan\nBirthday: April 2, 2011\nHobbies: Rowing, piano, and vibecoding\nFavorite Book: Wings of Fire\nFavorite movie: Spider-Man: Brand New Day");
+    }
+}
---
+++ BlockLetters.java
@@ -0,0 +1,15 @@
+public class BlockLetters {
+    public static void main(String[] args) {
+        System.out.println(" A");
+        System.out.println("A A");
+        System.out.println("AAA");
+        System.out.println("A A");
+        System.out.println("A A");
+        System.out.println("");
+        System.out.println("CCC");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("CCC");
+    }
+}
